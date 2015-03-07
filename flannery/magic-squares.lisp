e86da4d7699e865055f4c32e58567fcc0a13490b
Brian O'Callaghan <ocallaghan.brian.j@gmail.com>
2015-03-07 08:59:11 -0600

    Milestone. Can now test array for magic-ness

--- flannery/magic-squares.lisp
+++ flannery/magic-squares.lisp
@@ -32,7 +32,58 @@
   (let (rows
         (num-rows (array-dimension array 0))
         (num-cols (array-dimension array 1)))
-    (dotimes (row-index num-rows)
+    (dotimes (row-index num-rows (nreverse rows))
       (let ((row-vec (make-array num-cols)))
         (dotimes (col-index num-cols)
-          (vector 
+          (setf (aref row-vec col-index) (aref array row-index col-index)))
+        (push row-vec rows)))))
+                
+
+(defun columns (array)
+  "Retuns the rows of a 2d ARRAY as a sequence of vectors."
+  (let (cols
+        (num-rows (array-dimension array 0))
+        (num-cols (array-dimension array 1)))
+    (dotimes (col-index num-cols (nreverse cols))
+      (let ((col-vec (make-array num-rows)))
+        (dotimes (row-index num-rows)
+          (setf (aref col-vec row-index) (aref array row-index col-index)))
+        (push col-vec cols)))))
+
+(defun diagonals (array)
+  "Retuns the diagonals of a 2d square ARRAY as a sequence of two vectors."
+  (let ((num-rows (array-dimension array 0))
+        (num-cols (array-dimension array 1)))
+    (unless (= num-rows num-cols) (error "can only do diagonals of a square"))
+    (let ((diag-1 (make-array num-rows))
+          (diag-2 (make-array num-rows)))
+      (dotimes (row-index num-rows)
+        (setf (aref diag-1 row-index)
+              (aref array row-index row-index))
+        (setf (aref diag-2 row-index)
+              (aref array row-index (- num-rows row-index 1))))
+      (list diag-1 diag-2))))
+
+(defun fill-into-array (target-array num-list)
+  "Fills in a 2d array with the numbers in num-list"
+  (let ((nums (copy-seq num-list)))
+    (dotimes (row-index (array-dimension target-array 0) target-array)
+      (dotimes (col-index (array-dimension target-array 1))
+        (setf (aref target-array row-index col-index) (pop nums))))))
+
+;;; tests
+
+(defparameter *perfect-square* (make-array '(3 3)))
+(fill-into-array *perfect-square* '(8 1 6 3 5 7 4 9 2))
+(defparameter *imperfect-square* (make-array '(3 3)))
+(fill-into-array *imperfect-square* '(5 6 4 9 7 8 1 2 3))
+
+(ql:quickload :rt)
+
+(rt:deftest magic-square
+    (magic-square-p *perfect-square*) t)
+
+(rt:deftest non-magic-square
+    (magic-square-p *imperfect-square*) nil)
+
+(rt:do-tests)
