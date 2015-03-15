d6d3406453e63b7b6ed367ee2f8fd5455e92d099
Brian O'Callaghan <ocallaghan.brian.j@gmail.com>
2015-03-14 23:07:44 -0500

    start of Grand Theft Wumpus

---
+++ barski/wumpus.lisp
@@ -0,0 +1,18 @@
+(defparameter *city-nodes* nil)
+(defparameter *city-edges* nil)
+(defparameter *visited-nodes* nil)
+(defparameter *node-num* 30)
+(defparameter *edge-num* 45)
+(defparameter *worm-num* 3)
+(defparameter *cop-odds* 15) ; means 1 in 15
+
+(defun random-node ()
+  (1+ (random *node-num*)))
+
+(defun edge-pair (a b)
+  (unless (eql a b)
+    (list (cons a b) (cons b a))))
+
+(defun make-edge-list ()
+  (apply #'append (loop repeat *edge-num*
+                     collect (edge-pair (random-node) (random-node)))))
